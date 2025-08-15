3fbbd858b2148d66cb2f7034d5827156cf7e2ae8
vaidz <vaidheeshbharad@gmail.com>
2025-08-15 09:32:13 +0530

    fixed pali logic

--- Introductory-Problems/PalindromeReorder.cpp
+++ Introductory-Problems/PalindromeReorder.cpp
@@ -7,31 +7,31 @@ int main(){
 
     string s;
     cin >> s;
-    vector<int> freq (26,0);
-    for(const auto &t : s) freq[t - 'A']++;
+    vector<int> freq(26, 0);
+    for(char t : s) freq[t - 'A']++;
 
     int count = 0;
-    for(int f : freq) if(f%2 != 0) count++;
+    for(int f : freq) if(f % 2 != 0) count++;
 
     if(count > 1){
-        cout << "NO SOLUTION";
+        cout << "NO SOLUTION\n";
         return 0;
     }
 
     string first;
     char middle = 0;
 
-    for(int i = 0; i<26; ++i){
-        if(freq[i] % 2 == 0){
+    for(int i = 0; i < 26; ++i){
+        if(freq[i] % 2 != 0){
             middle = 'A' + i;
             freq[i]--;
         }
-        first.append(freq[i]/2, 'A'+i);
+        first.append(freq[i] / 2, 'A' + i);
     }
+
     string second_half = first;
     reverse(second_half.begin(), second_half.end());
 
-    if(middle) cout << first << middle << second_half;
-    else cout << first << second_half;
-
+    if(middle) cout << first << middle << second_half << '\n';
+    else cout << first << second_half << '\n';
 }
